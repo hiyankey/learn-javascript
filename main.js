0edd8de00f8a85c197cf7c6d541439a27bb8a732
Emmanuel Yankey <hiyankey@gmail.com>
2025-01-02 01:32:35 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import {test_bindings} from "./test_bindings.js"
+
+
+function main() {
+   test_bindings()
+}
+
+main()
---
+++ test_bindings.js
@@ -0,0 +1,7 @@
+export default {}
+export const num = 42
+export function test_bindings() {
+    const str = "Hello world!"
+    console.log(str)
+    console.log(num)
+}
